8391de2c7035e60b40984c170c6fc6d1bdac275c
Kim TaeSeon <deankim@mobigen.com>
2023-10-30 17:46:56 +0900

    first commit

---
+++ src/main/java/com/example/localevents/LocalEventsApplication.java
@@ -0,0 +1,13 @@
+package com.example.localevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalEventsApplication.class, args);
+	}
+
+}
